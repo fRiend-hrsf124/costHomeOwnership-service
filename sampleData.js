271319639263c376fa023265faac6091e60f82c9
nickzylstra <nick.zylstra@gmail.com>
2019-11-27 14:20:57 -0800

    Adds example data for db seeding modeling

---
+++ sampleData.js
@@ -0,0 +1,36 @@
+const properties = [
+  { 
+    property_id: 12345,
+    property_zip_code: 67890,
+    redfin_cost_estimate: 123456789,
+    property_tax_rate: 1.345,
+    insurance_rate: 0.234,
+  },
+];
+
+const rates = [
+  {
+    rate_id: 12345,
+    APR: 4.345,
+    fee_rate: 0.128,
+    years: 30,
+    type: 'fixed',
+    lender: 'Watermark Home Loans NMLS #1838',
+    cost_low: 234567890,
+    cost_high: 345678901,
+    zip_code: 23456,
+    origination_year: 2019,
+  },
+  {
+    rate_id: 23478,
+    APR: 4.345,
+    fee_rate: 0.128,
+    years: 30,
+    type: 'fixed',
+    lender: 'Watermark Home Loans NMLS #1838',
+    cost_low: 234567890,
+    cost_high: 345678901,
+    zip_code: 23458,
+    origination_year: 2019,
+  },
+] 
