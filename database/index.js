e243b33d28d5f86f597f215e27977c98e7fd2a89
nickzylstra <nick.zylstra@gmail.com>
2019-11-27 17:57:01 -0800

    Completes db and table schema creator script

--- database/index.js
+++ database/index.js
@@ -18,10 +18,23 @@ const createDBQuery = `
   CREATE DATABASE IF NOT EXISTS ${DB};
   USE ${DB};
   CREATE TABLE IF NOT EXISTS properties(
-
+    property_id INTEGER,
+    property_zip_code SMALLINT,
+    redfin_cost_estimate INTEGER,
+    property_tax_rate DECIMAL(5,3),
+    insurance_rate DECIMAL(5,3),
   );
-  CREATE TABLE IF NOT EXISTS properties(
-
+  CREATE TABLE IF NOT EXISTS rates(
+    rate_id INTEGER,
+    lending_zip_code SMALLINT,
+    APR DECIMAL(5,3),
+    fee_rate DECIMAL(5,3),
+    years TINYINT,
+    type VARCHAR(5),
+    lender VARCHAR(50),
+    cost_low INTEGER,
+    cost_high INTEGER,
+    origination_year YEAR,
   );
 `;
 
