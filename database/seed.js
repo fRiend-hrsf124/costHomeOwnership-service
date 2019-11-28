2a6018c1887971c6fa799f2434821063098165f6
nickzylstra <nick.zylstra@gmail.com>
2019-11-28 13:01:41 -0800

    Adds lenderlogo urls to seed file

--- database/seed.js
+++ database/seed.js
@@ -31,3 +31,10 @@ addTables(auth)
   .catch((err) => {
     console.log(err);
   });
+
+// TODO - figure out how to programmatically retrieve list of urls
+const lenderLogoUrls = [
+  'https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/10271_logo.gif',
+  'https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/10612_logo.gif',
+  'https://hrsf-fec-cho-lenderlogos.s3-us-west-1.amazonaws.com/7834_logo.gif',
+];
