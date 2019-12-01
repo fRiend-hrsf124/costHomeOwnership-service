7c7bcb48e945216b340bc3e555962c12e017b7db
nickzylstra <nick.zylstra@gmail.com>
2019-12-01 10:14:44 -0800

    Render react App.jsx

---
+++ client/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/extensions */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const path = require('path');
 module.exports = {
   entry: './client/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       {
-        test: /\.m?js$|\.m?jsx$/,
+        test: /.js$|.jsx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
