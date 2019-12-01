dc70230527fbadf4a37964293403e72c6ed142c9
nickzylstra <nick.zylstra@gmail.com>
2019-11-30 19:06:37 -0800

    Implement minimal server setup
    Serves static index.html page that references main.js

---
+++ server/server.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.listen(port, () => console.log(`server listening on port ${port}`));
