2f1dcf19aa2c8f74120d2f276849389eb54d87b6
nickzylstra <nick.zylstra@gmail.com>
2019-12-02 09:53:47 -0800

    Allow seedscript to be run before server

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 const mysql = require('mysql2/promise');
 const faker = require('faker/locale/en_US');
+const fs = require('fs');
+const path = require('path');
 const auth = require('./auth');
 
 const createDbConn = (scopeAuth) => {
@@ -15,6 +17,13 @@ const createDbConn = (scopeAuth) => {
     database,
     multipleStatements: true,
   });
+};
+
+const createDbTables = (conn) => {
+  const schemaFile = path.resolve(__dirname, 'schema.sql');
+  const createDBQuery = fs.readFileSync(schemaFile).toString();
+
+  return conn.query(createDBQuery);
 };
 
 const cleanDbTables = (conn) => {
@@ -167,6 +176,9 @@ const seedRates = (conn, zips) => {
   sharedZips = [...sharedZips];
 
   const conn = await createDbConn(scopeAuth);
+
+  await createDbTables(conn);
+  console.log('created database tables if non-existant');
 
   await cleanDbTables(conn);
   console.log('cleaned database tables');
