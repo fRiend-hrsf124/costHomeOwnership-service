64730babf161e76bd3d7d58123dd9fc4e5c82f84
nickzylstra <nick.zylstra@gmail.com>
2019-12-01 20:50:28 -0800

    Implement rates and properties GET routes

--- server/app.js
+++ server/app.js
@@ -14,16 +14,27 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.get('/api/costHomeOwnership/properties', async (req, res) => {
   const { id } = req.body;
   try {
-    const [rows] = await controller.getPropertyData(id);
-    res.end(JSON.stringify(rows[0]));
+    const [properties] = await controller.getPropertyData(id);
+    res.json(properties[0]);
   } catch (err) {
     console.log(err);
     res.status(400).end('server could not retrieve property data');
   }
 });
 
-app.get('/api/costHomeOwnership/rates', (req, res) => {
-  const { cost, zip } = req.body;
+app.get('/api/costHomeOwnership/rates', async (req, res) => {
+  const {
+    cost, zip, term, type, downPay, credit, origYear,
+  } = req.body;
+  try {
+    const [rates] = await controller.getRates(
+      cost, zip, term, type, downPay, credit, origYear,
+    );
+    res.json(rates);
+  } catch (err) {
+    console.log(err);
+    res.status(400).end('server could not retrieve rates data');
+  }
 });
 
 module.exports = app;
