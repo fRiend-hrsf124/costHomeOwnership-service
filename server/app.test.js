48535c2bfc69a6dfc00ca993643094ec4997ce69
nickzylstra <nick.zylstra@gmail.com>
2019-12-02 12:20:05 -0800

    Setup / and /api/cHO/properties test stubs

--- server/app.test.js
+++ server/app.test.js
@@ -1,5 +1,26 @@
-// const app = require('./app');
+const request = require('supertest');
+const app = require('./app');
 
-test('It adds two numbers', () => {
-  expect(1 + 1).toBe(2);
+describe('Server', () => {
+  describe('GET /', () => {
+    test('It should respond with 200', async () => {
+      const res = await request(app).get('/');
+      expect(res.statusCode).toBe(200);
+    });
+  });
+
+  describe('GET /api/costHomeOwnership/properties', () => {
+    test('It should respond with a JSON object with property_id key', async () => {
+      const body = { id: 7 };
+      const res = await request(app)
+        .get('/api/costHomeOwnership/properties')
+        .send(body);
+      expect(res.statusCode).toBe(200);
+      // expect(res.body).toBe(
+      //   expect.objectContaining({
+      //     property_id: 7,
+      //   }),
+      // );
+    });
+  });
 });
