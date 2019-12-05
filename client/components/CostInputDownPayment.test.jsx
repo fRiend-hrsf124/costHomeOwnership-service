96ca00f11f6d6c97129cf1715797fff116de97fa
nickzylstra <nick.zylstra@gmail.com>
2019-12-04 18:36:28 -0800

    Add tests for slider and dollar rendering

--- client/components/CostInputDownPayment.test.jsx
+++ client/components/CostInputDownPayment.test.jsx
@@ -8,7 +8,7 @@ describe('CostInputDownPayment', () => {
   const downPay = 20;
   const handleDownPaySubmit = () => { };
 
-  beforeEach(() => {
+  beforeAll(() => {
     downPayment = mount(<DownPayment
       cost={cost}
       downPay={downPay}
@@ -18,7 +18,16 @@ describe('CostInputDownPayment', () => {
 
   test('It should set passed in downPay prop to downPay input value', () => {
     const domDownPay = downPayment.find('#downPay').props().value;
-
     expect(domDownPay).toEqual(downPay);
+  });
+
+  test('It should set downPayDollars value correctly', () => {
+    const domDownPayDollars = downPayment.find('#downPayDollars').props().value;
+    expect(domDownPayDollars).toEqual((downPay / 100) * cost);
+  });
+
+  test('It should set downPaySlider value correctly', () => {
+    const domDownPaySlider = downPayment.find('#downPaySlider').props().value;
+    expect(domDownPaySlider).toEqual(downPay);
   });
 });
