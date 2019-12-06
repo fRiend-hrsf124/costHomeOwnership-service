1397b8775e9cd7f9667a7d1758ff306c3abc2b8c
nickzylstra <nick.zylstra@gmail.com>
2019-12-05 17:25:15 -0800

    Add CostInputs page layout

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 import React from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 import CostInputs from './components/CostInputs.jsx';
+
+const Container = styled.div`
+  max-width: 667px;
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -80,10 +85,12 @@ class App extends React.Component {
 
   handleCostSubmit(cost) {
     this.setState({ cost });
+    this.getRates();
   }
 
   handleDownPaySubmit(downPay) {
     this.setState({ downPay });
+    this.getRates();
   }
 
   render() {
@@ -99,7 +106,7 @@ class App extends React.Component {
       redfinCostEstimate,
     } = this.state;
     return (
-      <div>
+      <Container>
         <CostInputs
           key={cost * downPay}
           cost={cost}
@@ -108,7 +115,7 @@ class App extends React.Component {
           handleDownPaySubmit={this.handleDownPaySubmit}
           redfinCostEstimate={redfinCostEstimate}
         />
-      </div>
+      </Container>
     );
   }
 }
--- client/components/CostInputs.jsx
+++ client/components/CostInputs.jsx
@@ -1,7 +1,21 @@
 /* eslint-disable import/extensions */
 import React from 'react';
+import styled from 'styled-components';
 import HomePrice from './CostInputHomePrice.jsx';
 import DownPayment from './CostInputDownPayment.jsx';
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding-bottom: 24px;
+`;
+
+const SubContainer = styled.div`
+  width: 49%;
+  flex-basis: 49%;
+  margin-top: 10px;
+`;
 
 const CostInputs = (props) => {
   const {
@@ -13,18 +27,22 @@ const CostInputs = (props) => {
   } = props;
 
   return (
-    <div>
-      <HomePrice
-        cost={cost}
-        handleCostSubmit={handleCostSubmit}
-        redfinCostEstimate={redfinCostEstimate}
-      />
-      <DownPayment
-        cost={cost}
-        downPay={downPay}
-        handleDownPaySubmit={handleDownPaySubmit}
-      />
-    </div>
+    <Container>
+      <SubContainer>
+        <HomePrice
+          cost={cost}
+          handleCostSubmit={handleCostSubmit}
+          redfinCostEstimate={redfinCostEstimate}
+        />
+      </SubContainer>
+      <SubContainer>
+        <DownPayment
+          cost={cost}
+          downPay={downPay}
+          handleDownPaySubmit={handleDownPaySubmit}
+        />
+      </SubContainer>
+    </Container>
   );
 };
 
