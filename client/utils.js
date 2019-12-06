81d1c191c0a4cacbe37fafcfbd1cd94897c185fa
nickzylstra <nick.zylstra@gmail.com>
2019-12-06 13:07:23 -0800

    Add loan formatting util

--- client/utils.js
+++ client/utils.js
@@ -1,6 +1,7 @@
 import validator from 'validator';
 
 const formatNum = (num) => `$${new Intl.NumberFormat('en-US').format(num)}`;
+
 const parseUserStr = (str, prevNum) => {
   // TODO - sanitize input to prevent XSS
   if (str === '') return 0;
@@ -12,7 +13,19 @@ const parseUserStr = (str, prevNum) => {
   return nextNum;
 };
 
+const formatLoanType = (term, type) => {
+  let formattedLoan;
+  if (type === 'Fixed') {
+    formattedLoan = `${term} Year Fixed`;
+  } else {
+    formattedLoan = `${term}/1 ARM`;
+  }
+
+  return formattedLoan;
+};
+
 export {
   formatNum,
   parseUserStr,
+  formatLoanType,
 };
