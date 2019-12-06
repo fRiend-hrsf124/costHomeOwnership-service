022c0b78e5d1e01769677ab825fa61a34c4b254e
nickzylstra <nick.zylstra@gmail.com>
2019-12-06 11:40:06 -0800

    Stub Rates test

---
+++ client/components/Rates.test.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { shallow } from 'enzyme';
+import Rates from './Rates';
+
+describe('Rates', () => {
+  test('It should make actual tests', () => {
+    expect(true).toBe(true);
+  });
+});
