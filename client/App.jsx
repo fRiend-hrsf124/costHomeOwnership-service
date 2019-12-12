4c581ea71f1f05e5470af733b923bb313b611aa4
nickzylstra <nick.zylstra@gmail.com>
2019-12-12 09:30:23 -0800

    Refactor host assignment in client

--- client/App.jsx
+++ client/App.jsx
@@ -10,9 +10,10 @@ import { AppContainer } from './components/styles.jsx';
 
 // const hostLocal = 'http://localhost:3001';
 // const hostAWS = 'http://ec2-13-52-99-63.us-west-1.compute.amazonaws.com/';
-const url = window.location.href;
-const firstQIdx = url.indexOf('?');
-const host = url.substring(0, firstQIdx > 0 ? firstQIdx : url.length);
+// const url = window.location.href;
+// const firstQIdx = url.indexOf('?');
+// const host = url.substring(0, firstQIdx > 0 ? firstQIdx : url.length);
+const host = window.location.origin;
 
 class App extends React.Component {
   constructor(props) {
@@ -47,7 +48,7 @@ class App extends React.Component {
   async getPropertyData(id) {
     try {
       console.log(`requesting properties from '${host}'...`);
-      const res = await axios.get(`${host}api/costHomeOwnership/properties?id=${id}`);
+      const res = await axios.get(`${host}/api/costHomeOwnership/properties?id=${id}`);
       const {
         propertyId,
         zipCode,
