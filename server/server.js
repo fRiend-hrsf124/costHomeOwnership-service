339366f5a8baf32a7264bb00af2dab53fcc6225f
nickzylstra <nick.zylstra@gmail.com>
2019-12-13 11:03:09 -0800

    Add server retrieve bundle from s3 in prod

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
 /* eslint-disable no-console */
+const controller = require('./controller');
 const app = require('./app');
 
-const port = 3001;
-app.listen(port, () => console.log(`server listening on port ${port}`));
+(async () => {
+  if (process.env.NODE_ENV === 'prod') {
+    try {
+      await controller.updateClientBundle();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const port = 3001;
+  app.listen(port, () => console.log(`server listening on port ${port}`));
+})();
